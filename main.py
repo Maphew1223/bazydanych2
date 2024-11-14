e0acd1bc958270b7ab6eab8015e6e68d83b80a9c
Mateusz Nawrocki <nawrockim95@gmail.com>
2024-11-14 18:53:13 +0100

    add .

---
+++ main.py
@@ -0,0 +1,16 @@
+import sqlite3
+import pandas as pd
+
+stations_data = pd.read_csv("clean_stations.csv")
+measure_data = pd.read_csv("clean_measure.csv")
+
+print(stations_data)
+print(measure_data)
+
+conn = sqlite3.connect("database.db")
+
+stations_data.to_sql("stations", conn, if_exists="replace", index=False)
+measure_data.to_sql("measurements", conn, if_exists="replace", index=False)
+
+result = conn.execute("SELECT * FROM stations LIMIT 5").fetchall()
+print(result)
